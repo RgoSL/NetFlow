b18e59ded38080da6e875d186738c3780496a8e5
Rodrigo 🤙 <125403873+RgoSL@users.noreply.github.com>
2025-11-21 20:25:41 -0300

    chore: create initial project folder structure

--- app/src/main/java/com/example/netflow/ui/theme/Theme.kt
+++ app/src/main/java/com/example/netflow/ui/theme/Theme.kt
@@ -1,6 +1,5 @@
 package com.example.netflow.ui.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
