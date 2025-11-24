7b3e985c6c717f447820415ec7e38b5a0077f8b3
Rodrigo 🤙 <125403873+RgoSL@users.noreply.github.com>
2025-11-24 15:57:59 -0300

    feat: add AuthRepository to handle Firebase authentication

---
+++ app/src/main/java/com/example/netflow/auth/data/repository/AuthRepository.kt
@@ -0,0 +1,37 @@
+package com.example.netflow.auth.data.repository
+
+import com.google.firebase.auth.FirebaseAuth
+
+/* Classe da Definição de Métodos Para a Funcionalidade de Autenticação */
+
+class AuthRepository {
+
+    private  val auth = FirebaseAuth.getInstance() /* Variável Privada que Instância os Métodos do FirebaseAuth */
+
+    /* Criação de um Método Para o Login Utilizando um Objeto do Firebase */
+    fun login(email: String, password: String, onResult: (Boolean, String?) -> Unit) { /* Método Recebe Email e Senha Como Parâmetros */
+        auth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener {
+                /* Condicionais Para Confirmar os Dados de Login */
+                if (it.isSuccessful) onResult(true, null)
+                else onResult(false, it.exception?.message)
+            }
+    }
+
+    /* Criação de um Método Para o Cadastro de Usuários Utilizando um Objeto do Firebase */
+    fun register(email: String, password: String, onResult: (Boolean, String?) -> Unit) { /* Método Recebe Email e Senha Como Parâmetros */
+        auth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener {
+                /* Condicionais Para Salvar os Dados do Registro */
+                if (it.isSuccessful) onResult(true, null)
+                else onResult(false, it.exception?.message)
+            }
+    }
+    /* Criação de um Método Para Sair da Conta Atual Utilizando um Objeto do Firebase */
+    fun logout() {
+        auth.signOut()
+    }
+
+    /* Criação de um Método Para Incrementar um ID de Usuário Utilizando um Objeto do Firebase */
+    fun currentUserId(): String? = auth.currentUser?.uid
+}
